77a28d56c0f1a7697913d9a16dde9d7ae1f3de01
이승헌 <rltmsla5@naver.com>
2022-06-10 16:37:02 +0900

    0610
    0610

---
+++ FallGuysVSAi/Assets/2.scrip/Part1/Part1Gate.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Part1Gate : MonoBehaviour
+{
+    public GameObject gategard;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        //충동한 상대방 게임 오브젝트가 Part1Ball 태그를 가졌나요?
+        if(other.tag == "Part1Ball")
+        {
+            gategard.SetActive(false);
+            Debug.Log("충돌함");
+
+        } 
+    }
+   
+}
---
+++ FallGuysVSAi/Assets/2.scrip/PlayerInput.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerInput : MonoBehaviour
+{
+    public string moveAxisName = "Vertical"; //앞뒤 움직임을 위한 입력축 이름
+    public string rotareAxisName = "Horizontal"; //좌우 회전을 위한 입력축 이름
+    // public string jumpAxisName = "Jump";// 점프 를 위한 입력축이름
+
+    private int jumpCount = 0; // 점프 횟수
+    private Rigidbody playerRigidbody;
+    public float jumpForce = 700f; // 점프
+   
+    
+
+    public float move { get; private set; } // 감지된 움직임 입력값
+    public float rotate { get; private set; }// 감지된 회전 입력값
+    // public float jump {get;private set;}// 감지된 점프 입력값
+    
+    
+
+    void Start()
+    {
+        playerRigidbody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //게임오버 상태에서는 사용자 입력을 감지하지 않음
+    if(Gamemanager.instance!=null && Gamemanager.instance.isGameover)
+        {
+            move = 0;
+            rotate = 0;
+           
+          
+            return;
+        }
+    // 프로퍼티**
+        //move에 관한 입력 감지
+        move = Input.GetAxis(moveAxisName);
+        //rotate 에 관한 입력감지
+        rotate = Input.GetAxis(rotareAxisName);
+        //jump 에 관한 입력감지
+        // jump =Input.GetAxisRaw(jumpAxisName);
+    if(Input.GetKeyDown(KeyCode.Space))
+    {
+         
+
+            playerRigidbody.velocity = Vector2.zero;
+
+           
+            playerRigidbody.AddForce(new Vector2(0, jumpForce));
+            Debug.Log("점프");
+    }
+       
+        
+    if(Input.GetKeyDown(KeyCode.LeftShift))
+        {
+           GameObject.Find("Player").GetComponent<PlayerMovement>().moveSpeed=30f;
+        }  
+    if(Input.GetKeyUp(KeyCode.LeftShift))
+    {
+        GameObject.Find("Player").GetComponent<PlayerMovement>().moveSpeed=5f;
+    }
+
+    }
+
+
+}
